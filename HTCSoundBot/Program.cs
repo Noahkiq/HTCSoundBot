20fb8a50b3cb3c5fc35d0eb654802e3c6fdaceb9
Noahkiq <noahkiqgaming@gmail.com>
2016-11-07 16:08:05 -0500

    do coding

--- HTCSoundBot/Program.cs
+++ HTCSoundBot/Program.cs
@@ -5,13 +5,85 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Audio;
+using System.IO;
+using System.Timers;
+using NAudio;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
 
-namespace HTCSoundBot
+class Program
 {
-    class Program
+    static void Main(string[] args) => new Program().Start();
+
+    public static bool ohseriouslyEnabled = true;
+
+    private DiscordClient _client;
+
+    public void Start()
     {
-        static void Main(string[] args)
+        _client = new DiscordClient();
+
+        _client.UsingAudio(x => // Opens an AudioConfigBuilder so we can configure our AudioService
         {
-        }
+            x.Mode = AudioMode.Outgoing; // Tells the AudioService that we will only be sending audio
+        });
+
+        _client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
+
+
+        _client.MessageReceived += async (s, e) =>
+        {
+            if((e.Message.RawText.Contains("<:awseriously:245270309081448449>")) && (e.Channel.Id == 222494171506671616) && (ohseriouslyEnabled))
+            {
+                ohseriouslyEnabled = false;
+                System.Timers.Timer ohseriouslyTimer = new System.Timers.Timer();
+                ohseriouslyTimer.Elapsed += new ElapsedEventHandler(ohseriously);
+                ohseriouslyTimer.Interval = 3600000;
+                ohseriouslyTimer.Enabled = true;
+
+                var voiceChannel = _client.FindServers("HTwins Central").FirstOrDefault().FindChannels("[Memes] Sea of Davids").FirstOrDefault();
+
+                var _vClient = await _client.GetService<AudioService>() // We use GetService to find the AudioService that we installed earlier. In previous versions, this was equivelent to _client.Audio()
+                        .Join(voiceChannel); // Join the Voice Channel, and return the IAudioClient.
+
+                string filePath = $"{Directory.GetCurrentDirectory()}\\sounds\\ohseriously.mp3"; // Grab current directory
+
+                var channelCount = _client.GetService<AudioService>().Config.Channels; // Get the number of AudioChannels our AudioService has been configured to use.
+                var OutFormat = new WaveFormat(48000, 16, channelCount); // Create a new Output Format, using the spec that Discord will accept, and with the number of channels that our client supports.
+                using (var MP3Reader = new Mp3FileReader(filePath)) // Create a new Disposable MP3FileReader, to read audio from the filePath parameter
+                using (var resampler = new MediaFoundationResampler(MP3Reader, OutFormat)) // Create a Disposable Resampler, which will convert the read MP3 data to PCM, using our Output Format
+                {
+                    resampler.ResamplerQuality = 60; // Set the quality of the resampler to 60, the highest quality
+                    int blockSize = OutFormat.AverageBytesPerSecond / 50; // Establish the size of our AudioBuffer
+                    byte[] buffer = new byte[blockSize];
+                    int byteCount;
+
+                    while ((byteCount = resampler.Read(buffer, 0, blockSize)) > 0) // Read audio into our buffer, and keep a loop open while data is present
+                    {
+                        if (byteCount < blockSize)
+                        {
+                            // Incomplete Frame
+                            for (int i = byteCount; i < blockSize; i++)
+                                buffer[i] = 0;
+                        }
+                        _vClient.Send(buffer, 0, blockSize); // Send the buffer to Discord
+                    }
+                }
+
+                _vClient.Wait(); // Waits for the currently playing sound file to end.
+
+                await _vClient.Disconnect(); // Disconnects from the voice channel.
+            }
+        };
+
+        string token = File.ReadAllText("token.txt");
+        _client.ExecuteAndWait(async () => {
+            await _client.Connect(token, TokenType.Bot);
+        });
+
+    }
+    private static void ohseriously(object source, ElapsedEventArgs e)
+    {
+        ohseriouslyEnabled = true;
     }
 }
